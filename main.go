6086bba10df471abb946c8087b8cc7b585a46a4b
Wang Peng <pengpeng.wang@forceclouds.com>
2017-11-18 08:42:22 +0800

    修改main, add http service

--- main.go
+++ main.go
@@ -1,27 +1,16 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
 	"os"
-	"github.com/codegangsta/negroni"
+	"github.com/wangppp/shuffle/service"
 )
 
 func main() {
 	port := os.Getenv("PORT")
 	if len(port) == 0 {
-		port = "8080"
+		port = "3000"
 	}
-
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", hello)
-
-	n := negroni.Classic()
-	n.UseHandler(mux)
-	hostString := fmt.Sprintf(":%s", port)
-	n.Run(hostString)
+	 server := service.NewServer()
+	 server.Run(":" + port)
 }
 
-func hello(res http.ResponseWriter, req *http.Request) {
-	fmt.Fprintln(res, "Hello from PP NiNi world!")
-}
