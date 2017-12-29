e435d54384d715e328e0d0eba4cfc803753279f3
Wang Peng <pengpeng.wang@forceclouds.com>
2017-12-29 10:09:26 +0800

    add server and client judge

--- blog/utils/api/index.js
+++ blog/utils/api/index.js
@@ -1,9 +1,11 @@
 const fetch = require('axios')
-const baseUrl = process.env.APP_ENV !== 'production' ? 'http://localhost:5000/api/v1/public' : 'http://adminpanel.epic-think.xyz/api/v1/public'
+const isServer = typeof window === 'undefined' ? true : false
+const serverBaseUrl = 'http://goapp:4000/api/v1/public'
+const clientBaseUrl = 'http://adminpanel.epic-think.xyz/api/v1/public'
 // const baseUrl = 'http://localhost:5000/api/v1/public'
 
 function getUrl(url) {
-    return `${baseUrl}${url}`
+    return isServer ? `${serverBaseUrl}${url}` : `${clientBaseUrl}${url}`
 }
 
 export const getArticles = async function () {
